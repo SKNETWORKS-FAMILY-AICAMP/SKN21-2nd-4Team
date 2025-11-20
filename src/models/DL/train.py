fc32e2c7c226155a28a6015257f0d607771bd5e1
whskadnd <whskadnd25@naver.com>
2025-11-20 17:07:49 +0900

    streamlit수정1  및 딥러닝틀제작2

--- src/models/DL/train.py
+++ src/models/DL/train.py
@@ -1,13 +1,32 @@
 # 모델훈련
 ## 데이터 로딩, 모델 저장
 
-from model import MLPModel
+import pandas as pd
 import torch
+from torch.utils.data import DataLoader, TensorDataset
+from model import MLPModel
 import torch.optim as optim
-from torch.utils.data import DataLoader
+import torch.nn as nn
 
-# 데이터셋 로딩 (가정: 전처리된 데이터를 DataLoader에 넘겨받음)
-train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+# 데이터 불러오기
+train_data = pd.read_csv('data/processed/Customer_Churn_Dataset_0_impute_baseline.csv')
+
+# 숫자형 데이터만 선택
+train_data_numeric = train_data.select_dtypes(include=['float64', 'int64'])
+
+# 입력과 타깃 분리
+X = train_data_numeric.drop('Churn', axis=1).values  # Churn 컬럼 제외
+y = train_data_numeric['Churn'].values  # Churn 컬럼
+
+# 데이터 전처리 (Tensor로 변환)
+X_tensor = torch.tensor(X, dtype=torch.float32)
+y_tensor = torch.tensor(y, dtype=torch.float32)
+
+# TensorDataset 사용하여 (data, target) 형태로 묶기
+train_dataset = TensorDataset(X_tensor, y_tensor)
+
+# DataLoader 설정
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 
 # 모델 초기화
 model = MLPModel(input_size=10, hidden_size=50, output_size=1)
